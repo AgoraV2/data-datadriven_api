f6b9691c4281e5290c3634b23428fd6ba4ae8b6b
Frédéric BONÉ <frederic.bone@gmail.com>
2015-05-22 16:54:15 +0200

    add basic shared state feature:
     at init get HomeCinema value from other HomeCinema nodes already present on bus
     when HomeCinema value is modified by any node but self, update your homeCinema value but don't notifiy on bus
     when HomeCinema value is modified because of our ecosystem, update your homeCinema value and notifiy on bus

--- samples/agora/agora.cc
+++ samples/agora/agora.cc
@@ -16,17 +16,43 @@ using namespace gen::com_agora_v2;
 
 static char *myName;
 
-class A_HomeCinema :
-public datadriven::ProvidedObject, public HomeCinemaInterface {
+class MyHCListener :
+public datadriven::Observer<HomeCinemaProxy>::Listener {
 public:
-  A_HomeCinema(shared_ptr<datadriven::ObjectAdvertiser> advertiser,
-       bool on = false,
+  void OnUpdate(const shared_ptr<HomeCinemaProxy>& hc)
+  {
+    const datadriven::ObjectId& id = hc->GetObjectId();
+    const HomeCinemaProxy::Properties prop = hc->GetProperties();
+    
+    cout << "[HomeCinema listener] Update for hc " << id << ": location = "
+    << prop.Source.c_str() << ", state = " << prop.HomeCinema << endl;
+    cout << "> ";
+    cout.flush();
+    OnUpdateHC(prop.HomeCinema, prop.Source.c_str());
+  }
+
+  virtual void OnUpdateHC(const bool homeCinema, const char* source) = 0;
+};
+
+
+class A_HomeCinema :
+public datadriven::ProvidedObject, public HomeCinemaInterface, public MyHCListener {
+public:
+       shared_ptr<datadriven::Observer<HomeCinemaProxy> > observer;
+
+       A_HomeCinema(shared_ptr<datadriven::ObjectAdvertiser> advertiser,
+       bool defaultHomeCinemaState = false,
        qcc::String path = "") :
        datadriven::ProvidedObject(advertiser, path),   /* If you don't pass a path, it will be constructed for you */
        HomeCinemaInterface(this)
        {
-         HomeCinema = on;
+         HomeCinema = defaultHomeCinemaState;
          Source = myName;
+         observer = datadriven::Observer<HomeCinemaProxy>::Create(this);
+  
+         if (nullptr == observer) {
+            cerr << "Observer not correctly initialized !!!" << endl;
+         }
        }
        
        ~A_HomeCinema()
@@ -46,26 +72,21 @@ public:
          HomeCinema = true;
          Update();
        };
+
        void setOff(void) {
          HomeCinema = false;
          Update();
        };
-};
 
-class MyHCListener :
-public datadriven::Observer<HomeCinemaProxy>::Listener {
-public:
-  void OnUpdate(const shared_ptr<HomeCinemaProxy>& hc)
-  {
-    const datadriven::ObjectId& id = hc->GetObjectId();
-    const HomeCinemaProxy::Properties prop = hc->GetProperties();
-    
-    cout << "[HomeCinema listener] Update for hc " << id << ": location = "
-    << prop.Source.c_str() << ", state = " << prop.HomeCinema << endl;
-    cout << "> ";
-    cout.flush();
-    
-  }
+       void OnUpdateHC(const bool homeCinema, const char* source) {
+         // Update HomeCinema shared state value with the one updated from another Agora HomeCinema node
+         if(strncmp(source, myName, strlen(myName)) != 0) {
+           HomeCinema = homeCinema;
+           cout << "Update shared state value with: " << homeCinema << " coming from " << source <<  endl;
+           cout << "> ";
+           cout.flush();
+         }
+       }
 };
 
 int main(int argc, char** argv)
@@ -78,6 +99,10 @@ int main(int argc, char** argv)
     return 0;
   }
 
+  cout << "> ";
+  cout.flush();
+  
+
   shared_ptr<datadriven::ObjectAdvertiser> advertiser = datadriven::ObjectAdvertiser::Create();
   if (nullptr == advertiser) {
     cerr << "Object advertiser not correctly initialized !!!" << endl;
@@ -86,6 +111,11 @@ int main(int argc, char** argv)
   
   string path = "/" + string(myName);
   unique_ptr<A_HomeCinema> homeCinema = unique_ptr<A_HomeCinema>(new A_HomeCinema(advertiser, false, path.c_str()));
+
+  // Ugly method (find a way to be sure that we are registered on bus and be sure that all Alljoyn notifications have been received:
+  //   wait to be sure that OnUpdateHC is called if another HomeCinema node is already present 
+  //   on Agora bus to update your HomeCinema state value before putting object on bus
+  sleep(2);
     
   if (ER_OK == homeCinema->GetStatus()) {
     if (ER_OK != homeCinema->PutOnBus()) {
@@ -95,17 +125,8 @@ int main(int argc, char** argv)
     cerr << "Failed to construct a homeCinema properly" << endl;
   }
 
-  MyHCListener dl = MyHCListener();
-  shared_ptr<datadriven::Observer<HomeCinemaProxy> > observer = datadriven::Observer<HomeCinemaProxy>::Create(&dl);
-  
-  if (nullptr == observer) {
-    cerr << "Observer not correctly initialized !!!" << endl;
-    return EXIT_FAILURE;
-  }  
-  
   while (1) {
     string input;
-    cout << "> ";
     getline(cin, input);
     if (strncmp(input.c_str(),"exit",4)==0)
       break;
